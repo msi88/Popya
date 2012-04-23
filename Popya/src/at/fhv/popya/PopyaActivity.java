a36e25d93e1e43030e1820226d7b0daa29bf24ac
msi88 <michael_sieber@vol.at>
2012-04-23 09:29:05 +0200

    Checkstyle added.

--- Popya/src/at/fhv/popya/PopyaActivity.java
+++ Popya/src/at/fhv/popya/PopyaActivity.java
@@ -3,11 +3,19 @@ package at.fhv.popya;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 
+ * The PopyaActivity class.
+ * 
+ * @author Michael Sieber
+ */
 public class PopyaActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
