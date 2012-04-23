c036d65031cda11b9cb451b2d78d2b1d28a182ec
msi88 <michael_sieber@vol.at>
2012-04-23 08:48:52 +0200

    Initialize the project

---
+++ Popya/src/at/fhv/popya/PopyaActivity.java
@@ -0,0 +1,13 @@
+package at.fhv.popya;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PopyaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
