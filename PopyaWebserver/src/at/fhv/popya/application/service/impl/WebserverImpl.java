31840430a94b5fcc18ef9876a1e01434c43f3b46
msi88 <michael.sieber@students.fhv.at>
2012-05-13 08:58:17 +0200

    Started implementing the webserver. Added a timer task which removes old messages

--- PopyaWebserver/src/at/fhv/popya/application/service/impl/WebserverImpl.java
+++ PopyaWebserver/src/at/fhv/popya/application/service/impl/WebserverImpl.java
@@ -1,5 +1,11 @@
 package at.fhv.popya.application.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -7,6 +13,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import at.fhv.popya.application.service.IWebserver;
+import at.fhv.popya.application.service.timer.GarbageTimer;
 import at.fhv.popya.application.transfer.ConnectionTO;
 import at.fhv.popya.application.transfer.MessageSenderTO;
 import at.fhv.popya.application.transfer.MessageTO;
@@ -19,6 +26,17 @@ import com.sun.jersey.spi.resource.Singleton;
 @Singleton
 @Path("/popya")
 public class WebserverImpl implements IWebserver {
+	private static Map<UserTO, List<MessageTO<Object>>> _messages;
+	private static final int CLEAN_INTERVAL_MILLISEC = 5 * 60 * 1000;
+
+	static {
+		// list with all users and their received messages
+		_messages = new HashMap<UserTO, List<MessageTO<Object>>>();
+
+		// initialize the garbage timer for cleaning the messages
+		new Timer().schedule(new GarbageTimer(_messages), 1000,
+				CLEAN_INTERVAL_MILLISEC);
+	}
 
 	@POST
 	@Path("/connect")
@@ -26,9 +44,25 @@ public class WebserverImpl implements IWebserver {
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Override
 	public UsersTO connect(ConnectionTO connection) {
+		// register user
+		if (!_messages.containsKey(connection.getUser())) {
+			List<MessageTO<Object>> messageList = new ArrayList<MessageTO<Object>>();
+			_messages.put(connection.getUser(), messageList);
+		}
+
+		return getAllChatPartners(connection.getUser());
+	}
+
+	/**
+	 * Get all availabel chat partners for a specific user
+	 * 
+	 * @param user
+	 *            The user for whom all available chat partners should be loaded
+	 */
+	private UsersTO getAllChatPartners(UserTO user) {
+		// FIXME test implementation
 		UsersTO out = new UsersTO();
-		out.getUsers().add(connection.getUser());
-		// TODO implement
+		out.getUsers().add(user);
 		return out;
 	}
 
@@ -38,11 +72,13 @@ public class WebserverImpl implements IWebserver {
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Override
 	public MessagesTO<Object> getMessages(UserTO user) {
+
+		// get the messages
 		MessagesTO<Object> messages = new MessagesTO<Object>();
-		MessageTO<Object> message = new MessageTO<Object>(MessageTO.LANG_EN,
-				"My test message");
-		messages.getMessages().add(message);
-		// TODO implement
+		messages.setMessages(_messages.get(user));
+
+		// reset the message list
+		_messages.get(user).clear();
 		return messages;
 	}
 
@@ -52,8 +88,26 @@ public class WebserverImpl implements IWebserver {
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Override
 	public void sendMessage(MessageSenderTO message) {
-		// TODO Auto-generated method stub
+		for (UserTO receiver : _messages.keySet()) {
+			if (canCommunicate(receiver, message.getUser())) {
+				_messages.get(receiver).add(message.getMessage());
+			}
+		}
 
+	}
+
+	/**
+	 * Determine if two users can communicate with each other
+	 * 
+	 * @param receiver
+	 *            The potential receiver
+	 * @param sender
+	 *            The sender of the message
+	 * @return True if receiver and sender can communicate
+	 */
+	private boolean canCommunicate(UserTO receiver, UserTO sender) {
+		// TODO implement
+		return true;
 	}
 
 }
